58554d31ebb3cde41be9aeafd63cdbe893215b21
Alex Cartura <demonrose66@gmail.com>
2024-09-11 20:06:38 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let num1 = 5;
+
+let num2 = 10;
+
+let num3 = 8;
+
+let num4 = 20;
+
+let num5 = 6;
+
+
+let somma = num1 + num2 + num3 + num4 + num5;
+
+console.log(somma);
+
+let media = somma / 5;
+
+console.log('La somma tra i numeri equivale a ' +somma + ' e la media equivale a ' + media);
